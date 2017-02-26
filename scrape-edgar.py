93ba4fe3900b43db3e937241f05816172b376eed
Ubuntu <ubuntu@ip-172-31-39-120.us-west-2.compute.internal>
2017-02-26 13:45:36 +0000

    created subdir path navigation for crawl.idx files

--- scrape-edgar.py
+++ scrape-edgar.py
@@ -9,9 +9,23 @@ import gzip
 from lxml import etree
 import os
 
-rootdir = "~/projects/open-sec-data/data/crawler.idx"
+print("hello")
+
+
+def subdirs(path):
+    """Yield directory names not starting with '.' under given path."""
+    for entry in os.scandir(path):
+        if not entry.name.startswith('.') and entry.is_dir():
+            print(entry.name)
+            subdir_path = path + entry.name + "/"
+            for subdir in os.scandir(subdir_path):
+                qtr_path = subdir_path + subdir.name + "/"
+                print(subdir.name)
+                for idx in os.scandir(qtr_path):
+                    print(idx.name)
+                
+
 
 if __name__ == "__main__":
-    for subdir, dirs, files in os.walk(rootdir):
-        for file in files:
-            print(str(os.path.join(subdir, file)))
+    rootdir = "/home/ubuntu/projects/open-sec-data/data/crawler.idx/"
+    subdirs(rootdir)
