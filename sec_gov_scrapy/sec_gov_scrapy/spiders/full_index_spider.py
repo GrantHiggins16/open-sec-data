4d99e00fb067f1a5d7955524d9bb269a796f9970
elias <elias.jm.garcia@gmail.com>
2017-02-25 22:38:11 -0600

    nearly fully functioning parse_year and parse_quarters

--- sec_gov_scrapy/sec_gov_scrapy/spiders/full_index_spider.py
+++ sec_gov_scrapy/sec_gov_scrapy/spiders/full_index_spider.py
@@ -6,12 +6,22 @@ from sec_gov_scrapy.items import SecGovScrapyItem
 
 EDGAR_ARCHIVE = "https://www.sec.gov/Archives/edgar/full-index/"
 
+
 class FullIndexSpider(Spider):
-    print("name")
+
     name = "full-index.sec.gov"
+    working_url = ""
     # print("allowed_domains")
     # allowed_domains = ["sec.gov"]
     # start_urls = ["https://www.sec.gov/Archives/edgar/full-index/"]
+
+    def __init__(self, *a, **kw):
+        super(FullIndexSpider, self).__init__(*a, **kw)
+        self.working_url = ""
+
+    def set_url(self, url):
+        self.working_url = url
+        print("working_url: {}".format(self.working_url))
 
     def start_requests(self):
         print("start_urls")
@@ -31,7 +41,11 @@ class FullIndexSpider(Spider):
                 print(year)
                 item = SecGovScrapyItem()
                 item['url'] = year
-            yield item
+                quarters = EDGAR_ARCHIVE+year
+                # self.set_url(quarters)
+                print(response.url)
+                yield Request(quarters, callback=self.parse_quarters)
+                # yield item
             # item = SecGovScrapyItem()
             # item['url'] = year.xpath('@href').extract()
             # yield item
@@ -41,8 +55,16 @@ class FullIndexSpider(Spider):
         parser to extract quarters urls using a given years urls from
         parse_years
         """
-        response.selector.xpath(
+        quarters = response.selector.xpath(
             '//*[@id="main-content"]/table/tr/td/a/@href').extract()
+
+        for quarter in quarters:
+            print(quarter)
+            item = SecGovScrapyItem()
+            item['url'] = quarter
+            print(response.url)
+            # self.set_url(idx)
+            yield item
 
     # def parse(self, response):
     #     """
